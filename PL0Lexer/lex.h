05ad3dac5ba51b7888be73639713446663bf1eb0
fewihu <felix.mueller@htw-dresden.de>
2020-11-03 20:04:17 +0100

    PL0 Lexer mit Schlüsselworterkennung

---
+++ PL0Lexer/lex.h
@@ -0,0 +1,41 @@
+//Felix Müller 18-041-61
+//Lexer PL0
+
+//Funktionpointer für Aktionen
+typedef void (*funcPointer) (void);
+
+//Symbolcodes für zusammengesetzte Sonderzeichen und Schluesselworte
+typedef enum T_ZS{
+	zNIL,
+	zErg = 128, zLE, zGE, //:=   <=   >=
+	zBGN, zCLL, zCST, zDO, zEND, zIF, zODD, zPRC, zTHN, zVAR, zWHL
+}tZS;
+
+//Morphemcodes
+typedef enum T_MC {mcEmpty, mcSymb, mcNum, mcIdent}tMC;
+
+//Morphem-Struktur
+typedef struct{
+	tMC MC;			//Morphemcode
+	int posLine;	//Zeile
+	int posCol;		//Spalte
+	
+	union VAL{		//Wert abh. von Code
+		long Num;
+		char* pStr;
+		int Symb;
+	}Val;
+	
+	int MLen;		//Morphemlänge	
+}tMorph;
+
+
+//Fkt-prototypen
+int initLex();
+tMorph * lex();
+
+static void l(void);
+static void b(void);
+static void sl(void);
+static void gsl(void);
+static void slb(void);
